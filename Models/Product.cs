b233e87c309ff25488aec3a32ab1ad6ef4a0d8d8
ShaneeH <hahesyshane@gmail.com>
2022-07-28 05:34:03 +0100

    Add files via upload
    Models for the API

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend_.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Img { get; set; }
+        public string Category { get; set; }
+
+
+
+
+        public Product(int id, string name, int price, string img, string category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Img = img;
+            Category = category;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend_.Models
+{
+    public class User
+ {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Msg { get; set; }
+
+
+        public User(int id, string name, string msg){
+            Id = id;
+            Name = name;
+            Msg = msg;              
+        }
+    }
+}
