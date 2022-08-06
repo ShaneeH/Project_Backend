f5c6fa5bf5e623ce9a56ded523a81f45d3b96b35
ShaneeH <hahesyshane@gmail.com>
2022-08-06 19:15:41 +0100

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend_.Models
+{
+    public class Order
+    {
+        public int Order_id { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public string Products { get; set; }
+        public float Total { get; set; }
+
+
+
+        public Order(int order_id, string email, DateTime date, string products, float total)
+        {
+            Order_id = order_id;
+            Email = email;
+            Date = date;
+            Products = products;
+            Total = total;
+
+        }
+    }
+}
