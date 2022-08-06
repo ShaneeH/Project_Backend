7aa3bbd16ce25ba68706aa26e6faf0df6adfb7ac
ShaneeH <hahesyshane@gmail.com>
2022-08-06 18:53:04 +0100

    Add files via upload

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,23 +1,33 @@
 ﻿namespace Backend_.Models
 {
+
+
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public float Price { get; set; }
+        public string Brand { get; set; }
+        public string Desc { get; set; }
+        public string Type { get; set; }
         public string Img { get; set; }
-        public string Category { get; set; }
+
+        
 
 
 
 
-        public Product(int id, string name, int price, string img, string category)
+        public Product(int id, string name, float price, string brand, string desc, string type, string img)
         {
             Id = id;
             Name = name;
             Price = price;
+            Brand = brand;  
+            Desc = desc;    
+            Type = type;
             Img = img;
-            Category = category;
         }
     }
+
+
 }
