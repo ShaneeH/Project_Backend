cf8dd7ea58601329ed3acfa6ea903b652a04004f
ShaneeH <hahesyshane@gmail.com>
2022-12-20 02:08:40 +0000

    Add files via upload

---
+++ Models/UserData.cs
@@ -0,0 +1,21 @@
+﻿namespace Backend_.Models
+{
+
+
+    public class UserData
+    {
+        public string Brand { get; set; }
+        public int Clicks { get; set; }
+
+
+
+        public UserData(string brand, int clicks)
+        {
+            Brand = brand;
+            Clicks = clicks;
+ 
+        }
+    }
+
+
+}
